786ae1a0b0caf4421e4a4fa18a6ffba9362e60e4
Rodrigo Garcia-Novoa <rodrigogg400@yahoo.com>
2018-06-24 16:49:13 -0700

    Added Splash Screen

---
+++ Stack/Assets/Scripts/RateButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateButton : MonoBehaviour {
+
+	private string ANDROID_RATE_URL = "market://details?id=com.RGN.GAMES.Super.Stacker";
+
+	public void RateFunction() {
+		Application.OpenURL(ANDROID_RATE_URL);
+	}
+}
